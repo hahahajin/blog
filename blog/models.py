327c2225883a713ba52455e0cdf4a37c4afe387b
hahahajin <yhj010810@naver.com>
2021-05-17 18:39:50 +0900

    create static

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+
+    def summary(self):
+        return self.body[:100]
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
