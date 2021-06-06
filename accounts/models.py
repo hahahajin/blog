0d23af967c4d92416665ddbc25a95e66271be954
hahahajin <yhj010810@naver.com>
2021-06-06 18:59:31 +0900

    로그인 완성

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    username = models.CharField(max_length=100) 
+    user_id = models.CharField(max_length=100) 
     password = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.user_id
--- accounts/views.py
+++ accounts/views.py
@@ -4,14 +4,35 @@ from django.contrib.auth.hashers import make_password, check_password
 from .models import *
 from django.contrib import auth
 # Create your views here.
+
 def signup(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    re_password = request.POST['re-password']
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    elif request.method == 'POST':
+        
+            user_id = request.POST['user_id']
+            password = request.POST['password']
+            re_password = request.POST['re-password']
 
-           
-    return render(request, 'signup_done.html', {'message': '회원가입을 완료하였습니다.'})
+            res_data = {} #응답 메시지를 담을 변수(딕셔너리)
 
+            if not (user_id and password and re_password):
+                res_data['error'] = '모든 값을 입력해야 합니다.'
+                return render(request, 'signup.html', res_data)
+            elif password != re_password:
+                res_data['error'] = '비밀번호가 다릅니다'
+                return render(request, 'signup.html', res_data)
+            elif User.objects.filter(user_id = request.POST['user_id']).exists():
+                res_data['error'] = '이미 존재하는 아이디입니다.'
+                return render(request, 'signup.html', res_data)
+            else:
+                user = User( # 모델에서 생성한 User 클래스를 가져와 객체 생성
+                    user_id = user_id,
+                    password = make_password(password),
+                )
+
+                user.save() #데이터베이스에 저장
+                return render(request, 'signup_done.html', {'message': '회원가입을 완료하였습니다.'})
 
 def login(request):
     if request.method== 'GET':
@@ -29,10 +50,20 @@ def login(request):
         user = User.objects.get(user_id = user_id)
         if check_password(password, user.password):
             request.session['user'] = user.id
-            return redirect('main')
+            return redirect('index')
         else:
             res_data['error'] = '비밀번호가 틀렸습니다.'
     
             return render(request, 'login.html', res_data)
-    return redirect("main")
+    return redirect("index")
+
+def logout(request):
+    
+    if request.session.get('user'):
+        del(request.session['user'])
+        
+    return redirect('index')
+    
+def signup_done(request):
+    return render(request, 'signup_done.html')
 
